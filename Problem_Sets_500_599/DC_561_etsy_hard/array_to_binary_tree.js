debc5fd3b094abacdbd98981d9c53b6ffcae0836
mingliu68 <mingliudev@gmail.com>
2020-06-25 10:51:14 -0400

    additional notes.md

--- Problem_Sets_500_599/DC_561_etsy_hard/array_to_binary_tree.js
+++ Problem_Sets_500_599/DC_561_etsy_hard/array_to_binary_tree.js
@@ -5,7 +5,7 @@
 const { Node } = require("../../Data_Structures/Binary_Tree/binary_tree.js")
 
 function array_to_binary(array) {
-        
+    // empty array return null    
     if (array.length == 0) {   
         return null
     }
@@ -33,9 +33,17 @@ function array_to_binary(array) {
 
 let sorted1 = [2,3,4,5,6,7,8] 
 let sorted2 = [0,1,2,3,4,5,6,7,8,9] 
+let sorted3 = [1]
+let sorted4 = []
 
 let binary_tree1 = array_to_binary(sorted1)
 let binary_tree2 = array_to_binary(sorted2)
 
 console.log(binary_tree1)
 console.log(binary_tree2)
+
+let binary_tree3 = array_to_binary(sorted3)
+let binary_tree4 = array_to_binary(sorted4)
+
+console.log(binary_tree3)
+console.log(binary_tree4)
